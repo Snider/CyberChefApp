22915214880cc56f3aad5840fe7add51b7d0a156
Snider <snider@lt.hn>
2022-09-14 07:11:04 +0100

    adding everything worked on so far, for another dev to finish off this project.

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"embed"
+	"github.com/wailsapp/wails/v2/pkg/menu"
+	"github.com/wailsapp/wails/v2/pkg/menu/keys"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -14,7 +17,15 @@ func main() {
 	// Create an instance of the app structure
 	app := NewApp()
 
+	// BrowserOpenURL
+	AppMenu := menu.NewMenu()
+	FileMenu := AppMenu.AddSubmenu("File")
+	FileMenu.AddText("&Open", keys.CmdOrCtrl("o"), func(_ *menu.CallbackData) {
+		runtime.BrowserOpenURL(app.ctx, "http://localhost#recipe=To_Base64('A-Za-z0-9%2B/%3D')")
+	})
+
 	// Create application with options
+	//  wails build -obfuscate
 	err := wails.Run(&options.App{
 		Title:            "CyberChef",
 		Width:            1024,
@@ -22,6 +33,7 @@ func main() {
 		Assets:           assets,
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
+		Menu:             AppMenu,
 		Bind: []interface{}{
 			app,
 		},
